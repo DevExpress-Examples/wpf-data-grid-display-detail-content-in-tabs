5c3c9ff7be57f9b5e324b2518cf6384278b30cf7
DevExpressExampleBot <fakeEmail>
2021-09-01 19:03:51 +0300

    Source auto update [skip ci]

--- VB/WpfApplication18/MainWindow.xaml.vb
+++ VB/WpfApplication18/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.Linq
 Imports System.Text
@@ -20,6 +19,7 @@ Namespace WpfApplication18
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
@@ -37,7 +37,7 @@ Namespace WpfApplication18
 		End Sub
 		Private Function CreateGrid() As GridControl
 			Dim newGrid As New GridControl()
-            newGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
+			newGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
 			newGrid.View = New TableView()
 			TryCast(newGrid.View, TableView).AutoWidth = True
 			TryCast(newGrid.View, TableView).ShowGroupPanel = False
@@ -52,12 +52,12 @@ Namespace WpfApplication18
 
 			Dim detailGrid As New GridControl()
 			dataControlDetail.DataControl = detailGrid
-            detailGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
+			detailGrid.AutoGenerateColumns = AutoGenerateColumnsMode.AddNew
 			detailGrid.View.DetailHeaderContent = "Orders"
 			TryCast(detailGrid.View, TableView).ShowGroupPanel = False
 
 			Dim contentDetail As New ContentDetailDescriptor()
-			contentDetail.ContentTemplate = CType(FindResource("EmployeeNotes"), DataTemplate)
+			contentDetail.ContentTemplate = DirectCast(FindResource("EmployeeNotes"), DataTemplate)
 			contentDetail.HeaderContent = "Notes"
 
 			Dim tabDetail As New TabViewDetailDescriptor()
@@ -68,122 +68,132 @@ Namespace WpfApplication18
 		End Sub
 		#End Region ' #GridInCode
 
-		Private employees_Renamed As List(Of Employee)
+'INSTANT VB NOTE: The field employees was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private employees_Conflict As List(Of Employee)
 		Private ReadOnly Property Employees() As List(Of Employee)
 			Get
-				If employees_Renamed Is Nothing Then
+				If employees_Conflict Is Nothing Then
 					PopulateEmployees()
 				End If
-				Return employees_Renamed
+				Return employees_Conflict
 
 			End Get
 		End Property
 		Private Sub PopulateEmployees()
-			employees_Renamed = New List(Of Employee)()
-			employees_Renamed.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
-			employees_Renamed(0).Orders.Add(New Order("Suppliers Inc.", DateTime.Now, "TV", 20))
-			employees_Renamed(0).Orders.Add(New Order("Suppliers Inc.", DateTime.Now.AddDays(3), "Projector", 15))
-			employees_Renamed.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
-			employees_Renamed(1).Orders.Add(New Order("Suppliers Inc.", DateTime.Now.AddDays(1), "Blu-Ray Player", 10))
+			employees_Conflict = New List(Of Employee)()
+			employees_Conflict.Add(New Employee("Bruce", "Cambell", "Sales Manager", "Education includes a BA in psychology from Colorado State University in 1970.  He also completed 'The Art of the Cold Call.'  Bruce is a member of Toastmasters International."))
+			employees_Conflict(0).Orders.Add(New Order("Suppliers Inc.", DateTime.Now, "TV", 20))
+			employees_Conflict(0).Orders.Add(New Order("Suppliers Inc.", DateTime.Now.AddDays(3), "Projector", 15))
+			employees_Conflict.Add(New Employee("Cindy", "Haneline", "Vice President of Sales", "Cindy received her BTS commercial in 1974 and a Ph.D. in international marketing from the University of Dallas in 1981.  She is fluent in French and Italian and reads German.  She joined the company as a sales representative, was promoted to sales manager in January 1992 and to vice president of sales in March 1993.  Cindy is a member of the Sales Management Roundtable, the Seattle Chamber of Commerce, and the Pacific Rim Importers Association."))
+			employees_Conflict(1).Orders.Add(New Order("Suppliers Inc.", DateTime.Now.AddDays(1), "Blu-Ray Player", 10))
 		End Sub
 
 	End Class
 
 	Public Class Employee
-		Private firstName_Renamed As String
-		Private lastName_Renamed As String
-		Private title_Renamed As String
-		Private notes_Renamed As String
-		Private orders_Renamed As List(Of Order)
+'INSTANT VB NOTE: The field firstName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private firstName_Conflict As String
+'INSTANT VB NOTE: The field lastName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private lastName_Conflict As String
+'INSTANT VB NOTE: The field title was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private title_Conflict As String
+'INSTANT VB NOTE: The field notes was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private notes_Conflict As String
+'INSTANT VB NOTE: The field orders was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private orders_Conflict As List(Of Order)
 		Public Sub New(ByVal firstName As String, ByVal lastName As String, ByVal title As String, ByVal notes As String)
-			Me.firstName_Renamed = firstName
-			Me.lastName_Renamed = lastName
-			Me.title_Renamed = title
-			Me.notes_Renamed = notes
-			Me.orders_Renamed = New List(Of Order)()
+			Me.firstName_Conflict = firstName
+			Me.lastName_Conflict = lastName
+			Me.title_Conflict = title
+			Me.notes_Conflict = notes
+			Me.orders_Conflict = New List(Of Order)()
 		End Sub
 		Public Sub New()
 		End Sub
 		Public Property FirstName() As String
 			Get
-				Return firstName_Renamed
+				Return firstName_Conflict
 			End Get
 			Set(ByVal value As String)
-				firstName_Renamed = value
+				firstName_Conflict = value
 			End Set
 		End Property
 		Public Property LastName() As String
 			Get
-				Return lastName_Renamed
+				Return lastName_Conflict
 			End Get
 			Set(ByVal value As String)
-				lastName_Renamed = value
+				lastName_Conflict = value
 			End Set
 		End Property
 		Public Property Title() As String
 			Get
-				Return title_Renamed
+				Return title_Conflict
 			End Get
 			Set(ByVal value As String)
-				title_Renamed = value
+				title_Conflict = value
 			End Set
 		End Property
 		Public Property Notes() As String
 			Get
-				Return notes_Renamed
+				Return notes_Conflict
 			End Get
 			Set(ByVal value As String)
-				notes_Renamed = value
+				notes_Conflict = value
 			End Set
 		End Property
 		Public ReadOnly Property Orders() As List(Of Order)
 			Get
-				Return orders_Renamed
+				Return orders_Conflict
 			End Get
 		End Property
 	End Class
 
 	Public Class Order
-		Private supplier_Renamed As String
-		Private date_Renamed As DateTime
-		Private productName_Renamed As String
-		Private quantity_Renamed As Integer
+'INSTANT VB NOTE: The field supplier was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private supplier_Conflict As String
+'INSTANT VB NOTE: The field date was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private date_Conflict As DateTime
+'INSTANT VB NOTE: The field productName was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private productName_Conflict As String
+'INSTANT VB NOTE: The field quantity was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private quantity_Conflict As Integer
 		Public Sub New(ByVal supplier As String, ByVal [date] As DateTime, ByVal productName As String, ByVal quantity As Integer)
-			Me.supplier_Renamed = supplier
-			Me.date_Renamed = [date]
-			Me.productName_Renamed = productName
-			Me.quantity_Renamed = quantity
+			Me.supplier_Conflict = supplier
+			Me.date_Conflict = [date]
+			Me.productName_Conflict = productName
+			Me.quantity_Conflict = quantity
 		End Sub
 		Public Property Supplier() As String
 			Get
-				Return supplier_Renamed
+				Return supplier_Conflict
 			End Get
 			Set(ByVal value As String)
-				supplier_Renamed = value
+				supplier_Conflict = value
 			End Set
 		End Property
 		Public Property [Date]() As DateTime
 			Get
-				Return date_Renamed
+				Return date_Conflict
 			End Get
 			Set(ByVal value As DateTime)
-				date_Renamed = value
+				date_Conflict = value
 			End Set
 		End Property
 		Public Property ProductName() As String
 			Get
-				Return productName_Renamed
+				Return productName_Conflict
 			End Get
 			Set(ByVal value As String)
-				productName_Renamed = value
+				productName_Conflict = value
 			End Set
 		End Property
 		Public Property Quantity() As Integer
 			Get
-				Return quantity_Renamed
+				Return quantity_Conflict
 			End Get
 			Set(ByVal value As Integer)
-				quantity_Renamed = value
+				quantity_Conflict = value
 			End Set
 		End Property
 	End Class
