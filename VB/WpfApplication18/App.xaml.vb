02bfaa695a0388dd6d5332efe9eab99eb6a44c89
DevExpressExampleBot <fakeEmail>
2022-10-21 15:08:50 +0400

    Source auto update [skip ci]

---
+++ VB/WpfApplication18/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace WpfApplication18
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
